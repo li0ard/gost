81e72b0c21142371c4448f11fe4404467aba8dfe
Lizard <li0ard@proton.me>
2026-05-21 19:39:47 +0600

    build(npm): bump version

--- src/modes/index.ts
+++ src/modes/index.ts
@@ -5,3 +5,4 @@ export { ecb } from "./ecb.js";
 export { mac, mac_legacy, omac_acpkm } from "./mac.js";
 export { mgm } from "./mgm.js";
 export { ofb } from "./ofb.js";
+export { kexp15, kwp } from "./wrap.js";
